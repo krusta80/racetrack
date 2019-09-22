39ee45079c7c347f31b3c647407afd1fb40a9e39
John Gruska <johngruska@gmail.com>
2019-09-21 22:31:07 -0400

    add gpio board logic to server.py

--- server.py
+++ server.py
@@ -3,20 +3,52 @@ import tornado.websocket
 import tornado.ioloop
 import tornado.web
 import socket
-'''
-This is a simple Websocket Echo server that uses the Tornado websocket handler.
-Please run `pip install tornado` with python of version 2.7.9 or greater to install tornado.
-This program will echo back the reverse of whatever it recieves.
-Messages are output to the terminal for debuggin purposes. 
-''' 
+import time
+
+# Import SPI library (for hardware SPI) and MCP3008 library.
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+
+# Time and light thresholds
+LIGHT_THRESHOLD = 10
+TIME_THRESHOLD = 3
+
+# Software SPI configuration:
+CLK  = 18
+MISO = 23
+MOSI = 24
+CS   = 25
+mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+
+# Hardware SPI configuration:
+SPI_PORT   = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+
+def finished_lap(track_number, last_finished_time):
+    return mcp.read_adc(track_number) < LIGHT_THRESHOLD and time.time() - last_finished_time > TIME_THRESHOLD
  
 class WSHandler(tornado.websocket.WebSocketHandler):
     def open(self):
         print 'new connection'
       
     def on_message(self, message):
-        print 'message received:  %s' % message
-        # Reverse Message and send it back
+        print 'race starting:  %s' % message
+        # Start race and send back updates...
+        # Main program loop.
+        start_time = time.time()
+        lapcounts = [0, 0, 0]
+        laptimes = [start_time, start_time, start_time]
+
+        while True:
+            for i in range(0, 3):
+                if finished_lap(i, laptimes[i]):
+                    #print "Car " + str(i+1) + " finished lap " + str(lapcounts[i]+1) + " at " + str(laptimes[i] - start_time) + " seconds."
+                    self.write_message(str(i) + "," + str(lapcounts[i]) + "," + str(time.time() - laptimes[i]))
+                    laptimes[i] = time.time()
+                    lapcounts[i] = lapcounts[i] + 1
+            time.sleep(0.001)
+
         print 'sending back message: %s' % message[::-1]
         self.write_message(message[::-1])
  
